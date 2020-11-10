faa7103a60537b9bca30a136d65425867a822192
Gavin Rough <gavinrough@gmail.com>
2020-11-10 11:49:39 -0500

    Add stereo I/O and mix control

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-[Lilac Loop](https://library.vcvrack.com/LilacLoop) is a plugin for [VCV Rack](https://vcvrack.com/). As of [v1.0.0](https://github.com/grough/lilac-loop-vcv/releases/tag/v1.0.0) it contains just one module called _Looper_. If you'd like to report a bug or are having trouble using the plugin, please submit an [issue](https://github.com/grough/lilac-loop-vcv/issues).
+[Lilac Loop](https://library.vcvrack.com/LilacLoop) is a plugin for [VCV Rack](https://vcvrack.com/). As of [v1.0.1](https://github.com/grough/lilac-loop-vcv/releases/tag/v1.0.1) it contains just one module called _Looper_. If you'd like to report a bug or are having trouble using the plugin, please submit an [issue](https://github.com/grough/lilac-loop-vcv/issues).
 
 ## Module: _Looper_
 
-The Looper module emulates the recording style of a [live looping pedal](https://en.wikipedia.org/wiki/Live_looping). It allows you to record, play and overdub a mono or polyphonic signal using a multi-function toggle control comparable to the main foot switch on a looper pedal.
+The Looper module emulates the recording style of a [live looping pedal](https://en.wikipedia.org/wiki/Live_looping). It allows you to record, play and overdub a mono, stereo, or polyphonic signal using a multi-function toggle control comparable to the main foot switch on a looper pedal.
 
-[![An example patch showing how to record and loop a monophonic input signal](examples/lilac-looper-mono-example.png)](https://patchstorage.com/lilac-looper-mono-example/)
+[![An example patch showing how to record and loop a stereo input signal](examples/lilac-looper-stereo-example.png)](https://patchstorage.com/lilac-looper-stereo-example/)
 
 ### Get Started
 
 [Install the plugin](https://library.vcvrack.com/LilacLoop) and try one of the example patches:
 
-- [Basic Looping](https://patchstorage.com/lilac-looper-mono-example/) (seen above)
-- [Stereo looping](https://patchstorage.com/lilac-looper-stereo-example/)
-- [Multi-track looping](https://patchstorage.com/lilac-looper-multi-track-example/)
+- [Stereo looping](https://patchstorage.com/lilac-looper-stereo-example/) (seen above)
+- [Mono Looping](https://patchstorage.com/lilac-looper-mono-example/)
+- [Polyphonic looping](https://patchstorage.com/lilac-looper-multi-track-example/)
 
 Press the big button to toggle between record, play and overdub modes. A typical looping session might look like this:
 
@@ -24,29 +24,21 @@ Record → Play → Overdub → Play → Overdub → … → Stop → Play → �
 
 - **Toggle** is the big button that moves through the active stages of looping:
   - Record - create a first recording, setting the duration of the loop
-  - Overdub - add new material into the loop
-  - Play - continue listening to the loop without adding to it
+  - Play - listen to the loop without adding to it
+  - Overdub - add new material into the loop while playing
 - **Status lights** below the big button show when you're recording (red) and playing (green); The light blinks every time Looper reaches the starting point of the loop.
 - **Stop** stops the loop. Pressing toggle while stopped will restart the loop from the beginning.
 - **Erase** removes a recorded loop from memory irreversibly allowing you to record a new loop. Looper doesn't actively prevent "pops" or "clicks" when erasing the loop (in contrast to other mode changes which are smooth and quiet). You can avoid "pops" by stopping the loop before erasing it.
+- **Mix** changes the balance between your live input signal and the recorded loop
 - Use the "**After first loop…**" option in the module's context menu to choose which mode is toggled after recording your first loop. The default setting is "Play". The "Overdub" setting will continue recording after recording your first loop
 
-### Stereo and Multi-Track Recording
+### Multi-Track Recording
 
-Although Looper works in mono by default, it's capable of recording up to 16 tracks at once thanks to VCV Rack's [polyphonic cables](https://vcvrack.com/manual/Polyphony). To record a multi-channel loop, connect a polyphonic cable to the input and press the big toggle button to start recording. Each channel on the polyphonic input will record on a dedicated internal channel and play back on the corresponding output channel. Use VCV's [Merge](https://library.vcvrack.com/Fundamental/Merge) and [Split](https://library.vcvrack.com/Fundamental/Split) modules to manage polyphony.
-
-To record in **stereo**, simply use voices 1 & 2 of a polyphonic signal (as seen in the [stereo example](https://patchstorage.com/lilac-looper-stereo-example/)).
-
-⚠️ Note that every input channel you plan to record must be connected before you start recording a new loop ([issue #3](https://github.com/grough/lilac-loop-vcv/issues/3)). Additional input channels connected after the first loop is recorded will be ignored until you erase and start a new recording.
-
-### Input Monitoring
-
-Looper doesn't mix its input signal with its output. If you want to hear live input along with a recorded loop, connect your live input and Looper's output into a mixer.
+Looper's stereo inputs and outputs are [polyphonic](https://vcvrack.com/manual/Polyphony), meaning you can record up to 32 tracks in total (left and right, 16 channels each). Connect a polyphonic cable to an input and press the big toggle button to start recording. Each channel of polyphony on the input will record on a separate internal track and play back on the corresponding output channel. Use VCV's [Merge](https://library.vcvrack.com/Fundamental/Merge) and [Split](https://library.vcvrack.com/Fundamental/Split) modules to manage polyphony.
 
 ### Known Issues & Limitations
 
 If you'd like to report a bug or are having trouble using the plugin, please submit an [issue](https://github.com/grough/lilac-loop-vcv/issues). Following are some known issues that may be solved in a future release:
 
-- Number of polyphonic channels is constant per recording ([#3](https://github.com/grough/lilac-loop-vcv/issues/3))
 - Erasing a loop during playback makes a "pop" or "click" sound
 - Looper does not enforce a maximum loop length
--- src/Looper.cpp
+++ src/Looper.cpp
@@ -1,23 +1,30 @@
-#include "loop.hpp"
 #include "plugin.hpp"
 #include <math.h>
+
+#define MULTI 2      // Left, right
+#define POLY 16      // Polyphony channels per input/output
+#define MULTIPOLY 32 // Maximum number of loop tracks (MULTI * POLY)
 
 struct Looper : Module {
   enum ParamIds {
     MODE_TOGGLE_PARAM,
     STOP_BUTTON_PARAM,
     ERASE_BUTTON_PARAM,
+    MIX_PARAM,
     NUM_PARAMS,
   };
   enum InputIds {
     MODE_CV_INPUT,
     STOP_CV_INPUT,
     ERASE_CV_INPUT,
-    MAIN_INPUT,
+    LEFT_INPUT,
+    RIGHT_INPUT,
+    MIX_CV_INPUT,
     NUM_INPUTS,
   };
   enum OutputIds {
-    MAIN_OUTPUT,
+    LEFT_OUTPUT,
+    RIGHT_OUTPUT,
     NUM_OUTPUTS,
   };
   enum LightIds {
@@ -29,75 +36,183 @@ struct Looper : Module {
     NUM_LIGHTS,
   };
 
-  unsigned int channels = 1;
-  Loop loop;
-  Mode mode, nextMode;
+  enum Mode {
+    STOPPED,
+    RECORDING,
+    PLAYING,
+    OVERDUBBING,
+  };
+
+  unsigned int size = 0;
+  unsigned int position = 0;
+  unsigned int tracks[MULTI];
+  std::vector<float> loop[MULTIPOLY];
+  int start[MULTIPOLY];
+  unsigned int pos[MULTIPOLY];
+  Mode mode = STOPPED;
   bool overdubAfterRecord = false;
   float t = 0.f;
   float blinkTime = .1f;
+  float mix = 0.f;
+  engine::Input *ins[MULTI];
+  engine::Output *outs[MULTI];
+  dsp::SlewLimiter inputSmoother;
+  dsp::SlewLimiter outputSmoother;
   dsp::BooleanTrigger toggleTrigger;
   dsp::BooleanTrigger eraseTrigger;
   dsp::BooleanTrigger stopTrigger;
   dsp::ClockDivider lightDivider;
   dsp::ClockDivider logDivider;
-  dsp::PulseGenerator atZeroPulse;
+  dsp::PulseGenerator restartPulse;
   dsp::PulseGenerator togglePulse;
 
   Looper() {
     config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-    configParam(MODE_TOGGLE_PARAM, 0.f, 1.f, 0.f, "");
-    configParam(STOP_BUTTON_PARAM, 0.f, 1.f, 0.f, "");
-    configParam(ERASE_BUTTON_PARAM, 0.f, 1.f, 0.f, "");
+    configParam(MODE_TOGGLE_PARAM, 0.f, 1.f, 0.f, "Toggle");
+    configParam(STOP_BUTTON_PARAM, 0.f, 1.f, 0.f, "Stop");
+    configParam(ERASE_BUTTON_PARAM, 0.f, 1.f, 0.f, "Erase");
+    configParam(MIX_PARAM, -1.f, 1.f, 0.f, "Mix");
+
+    ins[0] = &inputs[LEFT_INPUT];
+    ins[1] = &inputs[RIGHT_INPUT];
+    outs[0] = &outputs[LEFT_OUTPUT];
+    outs[1] = &outputs[RIGHT_OUTPUT];
+
+    inputSmoother.setRiseFall(100.f, 50.f);
+    outputSmoother.setRiseFall(100.f, 50.f);
+
     lightDivider.setDivision(pow(2, 9));
     logDivider.setDivision(pow(2, 13));
+
+    for (size_t i = 0; i < MULTIPOLY; i++) {
+      pos[i] = 0;
+      start[i] = -1;
+    }
+  }
+
+  Mode getNextMode(bool overdubAfterRecord) {
+    if (mode == STOPPED && size == 0) {
+      return RECORDING;
+    }
+    if (mode == RECORDING) {
+      return overdubAfterRecord ? OVERDUBBING : PLAYING;
+    }
+    if (mode == PLAYING) {
+      return OVERDUBBING;
+    }
+    if (mode == OVERDUBBING) {
+      return PLAYING;
+    }
+    if (mode == STOPPED && size > 0) {
+      return PLAYING;
+    }
+    return mode;
   }
 
   void process(const ProcessArgs &args) override {
-
     bool toggleTriggered = toggleTrigger.process(params[MODE_TOGGLE_PARAM].getValue() + inputs[MODE_CV_INPUT].getVoltage() > 0.f);
     bool stopTriggered = stopTrigger.process(params[STOP_BUTTON_PARAM].getValue() + inputs[STOP_CV_INPUT].getVoltage() > 0.f);
     bool eraseTriggered = eraseTrigger.process(params[ERASE_BUTTON_PARAM].getValue() + inputs[ERASE_CV_INPUT].getVoltage() > 0.f);
+    float mix = math::clamp(params[MIX_PARAM].getValue() + inputs[MIX_CV_INPUT].getVoltage(), -1.f, 1.f);
 
     if (toggleTriggered) {
-      loop.toggle(overdubAfterRecord);
+      Mode next = getNextMode(overdubAfterRecord);
+      if (mode == STOPPED && next == PLAYING) {
+        position = 0;
+        for (size_t i = 0; i < MULTI; i++) {
+          for (size_t c = 0; c < tracks[i]; c++) {
+            int track = i * 16 + c;
+            if (start[track] == 0) {
+              pos[track] = 0;
+            } else {
+              pos[track] = size - start[track];
+            }
+          }
+        }
+      }
+      mode = next;
       togglePulse.trigger(blinkTime);
     }
 
     if (stopTriggered) {
-      loop.stop();
+      mode = STOPPED;
     }
 
     if (eraseTriggered) {
-      loop.erase();
+      mode = STOPPED;
+      position = 0;
+      size = 0;
+      for (size_t i = 0; i < MULTI; i++) {
+        tracks[i] = 0;
+        for (size_t c = 0; c < 16; c++) {
+          int track = i * 16 + c;
+          loop[track].clear();
+          pos[track] = 0;
+          start[track] = -1;
+        }
+      }
     }
 
-    mode = loop.getMode();
-    nextMode = loop.getNextMode(overdubAfterRecord);
-
-    if (toggleTriggered && mode == RECORDING) {
-      channels = inputs[MAIN_INPUT].getChannels();
-      outputs[MAIN_OUTPUT].setChannels(channels);
-      loop.setChannels(channels);
+    float inputGate = inputSmoother.process(args.sampleTime, mode == RECORDING || mode == OVERDUBBING ? 1.f : 0.f);
+    float outputGate = outputSmoother.process(args.sampleTime, mode == STOPPED ? 0.f : 1.f);
+    float monitorLevel = mix > 0 ? 1 - mix : 1;
+    float loopLevel = mix > 0 ? 1 : 1 + mix;
+    if (mode == RECORDING) {
+      size++;
     }
-
-    outputs[MAIN_OUTPUT].writeVoltages(loop.process(args.sampleTime, inputs[MAIN_INPUT].getVoltages()));
-
-    if (loop.atZero()) {
-      atZeroPulse.trigger(blinkTime);
+    // Process each input (left, right)
+    for (size_t i = 0; i < MULTI; i++) {
+      int channels = ins[i]->getChannels();
+      if (channels > tracks[i] || size == 0) {
+        tracks[i] = channels;
+        outputs[i].setChannels(channels);
+      }
+      // Process each polyphony channel
+      for (size_t channel = 0; channel < tracks[i]; channel++) {
+        int track = i * 16 + channel;
+        float in = ins[i]->getVoltage(channel);
+        float out;
+        if (start[track] == -1 && size > 0) {
+          start[track] = position;
+        }
+        if (loop[track].size() < size) {
+          loop[track].push_back(0.f);
+        }
+        if (loop[track].empty()) {
+          out = monitorLevel * in;
+        } else {
+          out = monitorLevel * in + loopLevel * outputGate * loop[track][pos[track]];
+          loop[track][pos[track]] += inputGate * in;
+          pos[track]++;
+        }
+        if (pos[track] == size) {
+          pos[track] = 0;
+        }
+        outs[i]->setVoltage(out, channel);
+      }
+    }
+    if (size > 0) {
+      position++;
+    }
+    if (position == size) {
+      position = 0;
+    }
+    if (position == 0) {
+      restartPulse.trigger(blinkTime);
     }
     // Blink when loop restarts, but not right after toggling
-    float atZeroBlink = (1.f - togglePulse.process(args.sampleTime)) * atZeroPulse.process(args.sampleTime);
+    float restartBlink = (1.f - togglePulse.process(args.sampleTime)) * restartPulse.process(args.sampleTime);
 
     if (lightDivider.process()) {
       lights[RECORD_STATUS_LIGHT].setBrightness(0.f);
       lights[PLAY_STATUS_LIGHT].setBrightness(0.f);
       if (mode == RECORDING || mode == OVERDUBBING) {
-        lights[RECORD_STATUS_LIGHT].setBrightness(1.f - atZeroBlink * .5f);
+        lights[RECORD_STATUS_LIGHT].setBrightness(1.f - restartBlink * .5f);
       }
       if (mode == PLAYING || mode == OVERDUBBING) {
-        lights[PLAY_STATUS_LIGHT].setBrightness(1.f - atZeroBlink);
+        lights[PLAY_STATUS_LIGHT].setBrightness(1.f - restartBlink);
       }
-      if (mode == STOPPED && !loop.empty()) {
+      if (mode == STOPPED && size > 0) {
         float w = sin(6.f * M_PI * t) / 2 + 0.5f;
         lights[PLAY_STATUS_LIGHT].setBrightness(w / 3.f);
       }
@@ -109,16 +224,22 @@ struct Looper : Module {
 struct LargeWarmButton : SvgSwitch {
   LargeWarmButton() {
     momentary = true;
-    addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/button_warm_large_0.svg")));
-    addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/button_warm_large_1.svg")));
+    addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/BigButton_0.svg")));
+    addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/BigButton_1.svg")));
   }
 };
 
 struct WarmButton : SvgSwitch {
   WarmButton() {
     momentary = true;
-    addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/button_warm_medium_0.svg")));
-    addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/button_warm_medium_1.svg")));
+    addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/Button_0.svg")));
+    addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/Button_1.svg")));
+  }
+};
+
+struct WarmKnob : Davies1900hKnob {
+  WarmKnob() {
+    setSvg(APP->window->loadSvg(asset::plugin(pluginInstance, "res/Knob.svg")));
   }
 };
 
@@ -140,19 +261,23 @@ struct LooperWidget : ModuleWidget {
     addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
     addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-    addParam(createParamCentered<LargeWarmButton>(mm2px(Vec(12.7, 28.687)), module, Looper::MODE_TOGGLE_PARAM));
-    addParam(createParamCentered<WarmButton>(mm2px(Vec(18.266, 68.457)), module, Looper::STOP_BUTTON_PARAM));
-    addParam(createParamCentered<WarmButton>(mm2px(Vec(18.47, 89.149)), module, Looper::ERASE_BUTTON_PARAM));
+    addParam(createParamCentered<LargeWarmButton>(mm2px(Vec(12.7, 21.135)), module, Looper::MODE_TOGGLE_PARAM));
+    addParam(createParamCentered<WarmButton>(mm2px(Vec(18.266, 50.015)), module, Looper::STOP_BUTTON_PARAM));
+    addParam(createParamCentered<WarmButton>(mm2px(Vec(18.47, 65.87)), module, Looper::ERASE_BUTTON_PARAM));
+    addParam(createParamCentered<WarmKnob>(mm2px(Vec(18.47, 81.725)), module, Looper::MIX_PARAM));
 
-    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.761, 44.215)), module, Looper::MODE_CV_INPUT));
-    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.761, 68.457)), module, Looper::STOP_CV_INPUT));
-    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.761, 89.149)), module, Looper::ERASE_CV_INPUT));
-    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.761, 112.651)), module, Looper::MAIN_INPUT));
+    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.658, 34.16)), module, Looper::MODE_CV_INPUT));
+    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.658, 50.015)), module, Looper::STOP_CV_INPUT));
+    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.658, 65.87)), module, Looper::ERASE_CV_INPUT));
+    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.658, 81.725)), module, Looper::MIX_CV_INPUT));
+    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.658, 98.034)), module, Looper::LEFT_INPUT));
+    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(18.758, 98.034)), module, Looper::RIGHT_INPUT));
 
-    addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(18.657, 112.429)), module, Looper::MAIN_OUTPUT));
+    addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(6.658, 112.24)), module, Looper::LEFT_OUTPUT));
+    addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(18.758, 112.24)), module, Looper::RIGHT_OUTPUT));
 
-    addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(14.981, 45.273)), module, Looper::RECORD_STATUS_LIGHT));
-    addChild(createLightCentered<MediumLight<GreenLight>>(mm2px(Vec(20.54, 45.273)), module, Looper::PLAY_STATUS_LIGHT));
+    addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(14.981, 35.219)), module, Looper::RECORD_STATUS_LIGHT));
+    addChild(createLightCentered<MediumLight<GreenLight>>(mm2px(Vec(20.54, 35.219)), module, Looper::PLAY_STATUS_LIGHT));
   }
 
   void appendContextMenu(Menu *menu) override {
