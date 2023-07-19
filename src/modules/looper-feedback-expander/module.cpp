5f913b791db8ace1cfbd45fa51700f2b7796c225
Gavin Rough <214106+grough@users.noreply.github.com>
2023-07-19 15:13:08 -0400

    Revise feedback exapnder panel

--- src/modules/looper-feedback-expander/module.cpp
+++ src/modules/looper-feedback-expander/module.cpp
@@ -7,9 +7,9 @@ struct LooperFeedbackExpander : Module {
     PARAMS_LEN
   };
   enum InputId {
+    FEEDBACK_CV_INPUT,
     RETURN_1_INPUT,
     RETURN_2_INPUT,
-    FEEDBACK_CV_INPUT,
     INPUTS_LEN
   };
   enum OutputId {
@@ -18,18 +18,19 @@ struct LooperFeedbackExpander : Module {
     OUTPUTS_LEN
   };
   enum LightId {
+    BYPASS_LIGHT,
     LIGHTS_LEN
   };
 
   LooperFeedbackExpander() {
     config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-    configParam(BYPASS_PARAM, 0.f, 1.f, 0.f, "");
     configParam(FEEDBACK_PARAM, 0.0f, 1.0f, 1.0f, "Feedback", "%", 0.0f, 100.0f);
     configInput(RETURN_1_INPUT, "Left return");
     configInput(RETURN_2_INPUT, "Right return");
     configInput(FEEDBACK_CV_INPUT, "Feedback attenuator");
     configOutput(SEND_1_OUTPUT, "Left send");
     configOutput(SEND_2_OUTPUT, "Right send");
+    // configSwitch(BYPASS_PARAM, 0.f, 1.f, 1.f, "Bypass", {"On", "Off"});
   }
 };
 
@@ -43,15 +44,16 @@ struct LooperFeedbackExpanderWidget : ModuleWidget {
     addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
     addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-    // addParam(createParamCentered<WarmButton>(mm2px(Vec(7.62, 18.505)), module, LooperFeedbackExpander::BYPASS_PARAM));
-    addParam(createParamCentered<WarmKnob>(mm2px(Vec(7.62, 39.159)), module, LooperFeedbackExpander::FEEDBACK_PARAM));
+    addParam(createParamCentered<WarmKnob>(mm2px(Vec(7.62, 37.196)), module, LooperFeedbackExpander::FEEDBACK_PARAM));
 
-    addInput(createInputCentered<LilacPort>(mm2px(Vec(7.62, 52.338)), module, LooperFeedbackExpander::FEEDBACK_CV_INPUT));
-    addInput(createInputCentered<LilacPort>(mm2px(Vec(7.538, 100.677)), module, LooperFeedbackExpander::RETURN_1_INPUT));
-    addInput(createInputCentered<LilacPort>(mm2px(Vec(7.538, 112.777)), module, LooperFeedbackExpander::RETURN_2_INPUT));
+    addInput(createInputCentered<LilacPort>(mm2px(Vec(7.62, 49.841)), module, LooperFeedbackExpander::FEEDBACK_CV_INPUT));
+    addInput(createInputCentered<LilacPort>(mm2px(Vec(7.62, 100.267)), module, LooperFeedbackExpander::RETURN_1_INPUT));
+    addInput(createInputCentered<LilacPort>(mm2px(Vec(7.62, 112.367)), module, LooperFeedbackExpander::RETURN_2_INPUT));
 
-    addOutput(createOutputCentered<LilacPort>(mm2px(Vec(7.538, 69.986)), module, LooperFeedbackExpander::SEND_1_OUTPUT));
-    addOutput(createOutputCentered<LilacPort>(mm2px(Vec(7.538, 82.086)), module, LooperFeedbackExpander::SEND_2_OUTPUT));
+    addOutput(createOutputCentered<LilacPort>(mm2px(Vec(7.62, 69.047)), module, LooperFeedbackExpander::SEND_1_OUTPUT));
+    addOutput(createOutputCentered<LilacPort>(mm2px(Vec(7.62, 81.155)), module, LooperFeedbackExpander::SEND_2_OUTPUT));
+
+    // addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<GreenLight>>>(mm2px(Vec(7.62, 18.514)), module, LooperFeedbackExpander::BYPASS_PARAM, LooperFeedbackExpander::BYPASS_LIGHT));
   }
 };
 
